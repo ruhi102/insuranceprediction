d97d82a378d2bdaeccdc63480b051a43850e9b6d
ruhi102 <ruhi.rownak@gmail.com>
2024-04-21 10:22:08 -0400

    Initial commit

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('prediction', views.prediction, name='prediction'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+]
+
---
+++ home/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, HttpResponse
+import joblib
+
+model = joblib.load('static/random_forest_regressor')
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def register(request):
+    return render(request, 'register.html')
+def login(request):
+    return render(request, 'login.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def prediction(request):
+    if request.method == "POST":
+        # print("enter into the post request")
+        age = int(request.POST.get('age'))
+        sex = int(request.POST.get('sex'))
+        bmi = int(request.POST.get('bmi'))
+        children = int(request.POST.get('children'))
+        smoker = int(request.POST.get('smoker'))
+        region = int(request.POST.get('region'))
+
+        print(age, bmi, sex, children, smoker, region)
+
+        pred = round(model.predict([[age, sex, bmi, children, smoker, region]])[0])
+
+        # print(pred)
+
+        output = {
+            "output": pred
+        }
+
+        return render(request, 'prediction.html', output)
+    else:
+        return render(request, 'prediction.html')
